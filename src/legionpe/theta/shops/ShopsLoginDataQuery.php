f342ea059634029c3b9c0289c8f731ed27abe958
PEMapModder <pemapmodder1970@gmail.com>
2015-11-06 19:43:53 +0800

    LibKit update

--- src/legionpe/theta/shops/ShopsLoginDataQuery.php
+++ src/legionpe/theta/shops/ShopsLoginDataQuery.php
@@ -15,13 +15,34 @@
 
 namespace legionpe\theta\shops;
 
+use legionpe\theta\libkit\GlobalKit;
 use legionpe\theta\query\LoginDataQuery;
 
 class ShopsLoginDataQuery extends LoginDataQuery{
-	public function fetchPurchases(){
-		return true;
+	protected function onAssocFetched(\mysqli $db, &$row){
+		parent::onAssocFetched($db, $row);
+//		$row["products"] = $this->queryProducts($db, (int) $row["uid"]);
+//		$row["kits"] = $this->queryKits($db, (int) $row["uid"]);
 	}
-	public function fetchKits(){
-		return true;
+	protected function queryKits(\mysqli $db, $uid){
+		$kits = []; // Kit $products[$class][$kitid]
+		$result = $db->query("SELECT category,slot,name,value FROM kits WHERE uid=$uid");
+		$rows = []; // $rows[$class][$kitid][$category][$slot]
+		while(is_array($row = $result->fetch_assoc())){
+			$class = (int) $row["class"];
+			$kitid = (int) $row["kitid"];
+			$category = (int) $row["category"];
+			$slot = (int) $row["slot"];
+			$rows[$class][$kitid][$category][$slot] = $row;
+		}
+		$result->close();
+		foreach($rows as $class => $kits){
+			foreach($kits as $kitid => $cats){
+				$kit = new GlobalKit($class, $kitid);
+				$kit->importCategorizedRows($cats);
+			}
+		}
+		return $kits;
 	}
+
 }
